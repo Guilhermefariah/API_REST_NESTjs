4fc1c00572bd5a53c81e11cc5cc26e09ed53328a
Guilhermefariah <guilhermefaria321@gmail.com>
2024-09-10 08:40:42 -0300

    nova class BookDTO

---
+++ src/DTO/books.dto.ts
@@ -0,0 +1,10 @@
+export class BookDTO {
+
+    readonly name: string
+    readonly author: string[]
+    readonly language: string
+    readonly releaseYear: number
+    readonly publisher: string
+    readonly pages: number
+
+}
