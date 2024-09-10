a08e876abd075b35d370cde4f956d02deedc0cd0
Guilhermefariah <guilhermefaria321@gmail.com>
2024-09-10 09:56:57 -0300

    adicionando all decorators

--- src/DTO/books.dto.ts
+++ src/DTO/books.dto.ts
@@ -1,4 +1,9 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator"
+import { IsNotEmpty,
+         IsNumber,
+         IsPositive,
+         IsString,
+         MaxLength,
+         MinLength } from "class-validator"
 
 export class BookDTO {
 
@@ -8,10 +13,31 @@ export class BookDTO {
     @MaxLength(100)
     readonly name: string
 
+    @IsNotEmpty()
+    @IsString()
     readonly author: string[]
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(100)
     readonly language: string
+
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
     readonly releaseYear: number
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(100)
     readonly publisher: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
     readonly pages: number
 
 }
